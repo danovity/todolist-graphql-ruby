bbe77730c4900bd337320a59a5136e90c65c420a
Sean <sean.xiaoxu.ye@gmail.com>
2020-04-12 15:18:40 -0400

    added complete_task mutation

---
+++ app/graphql/mutations/complete_task.rb
@@ -0,0 +1,18 @@
+module Mutations
+  class CompleteTask < BaseMutation
+    # often we will need input types for specific mutation
+    # in those cases we can define those input types in the mutation class itself\
+    description "Mark the task as completed"
+
+    argument :task_id, ID, required: true
+
+    field :task, TypesTask::TaskType, null: false
+
+    def resolve(task_id: nil)
+      task = Task.find_by(task_id)
+      task&.completed!
+
+      {task: task}
+    end
+  end
+end
