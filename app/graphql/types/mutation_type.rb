9e18619b7603c8d34a5edb46e55902cb6bc923e2
Sean <sean.xiaoxu.ye@gmail.com>
2020-04-12 15:25:51 -0400

    added edit_subtask and delete_subtask mutations to mutation_type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,8 +4,10 @@ module Types
     field :signin_user, mutation: Mutations::SignInUser
     field :create_task, mutation: Mutations::CreateTask
     field :edit_task, mutation: Mutations::EditTask
+    field :edit_subtask, mutation: Mutations::EditSubtask
     field :complete_task, mutation: Mutations::CompleteTask
     field :delete_task, mutation: Mutations::DeleteTask
+    field :delete_subtask, mutation: Mutations::DeleteSubtask
     field :create_subtask, mutation: Mutations::CreateSubtask
     field :complete_subtask, mutation: Mutations::CompleteSubtask
   end
