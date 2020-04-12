9258235b7b77fa3f56ff8f5ca141695945faf71b
Sean <sean.xiaoxu.ye@gmail.com>
2020-04-12 15:22:52 -0400

    added mutation_type that contains different mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,12 @@
 module Types
   class MutationType < Types::BaseObject
-    field :create_user, mutation: Mutations::CreateUser
+    field :register_user, mutation: Mutations::RegisterUser
     field :signin_user, mutation: Mutations::SignInUser
+    field :create_task, mutation: Mutations::CreateTask
+    field :edit_task, mutation: Mutations::EditTask
+    field :complete_task, mutation: Mutations::CompleteTask
+    field :delete_task, mutation: Mutations::DeleteTask
+    field :create_subtask, mutation: Mutations::CreateSubtask
+    field :complete_subtask, mutation: Mutations::CompleteSubtask
   end
 end
