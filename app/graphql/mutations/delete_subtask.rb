2e1bdff14182f9b34edc840969844e1ae4a85d67
Sean <sean.xiaoxu.ye@gmail.com>
2020-04-12 15:20:56 -0400

    added delete_subtask mutation

---
+++ app/graphql/mutations/delete_subtask.rb
@@ -0,0 +1,17 @@
+module Mutations
+  class DeleteSubtask < BaseMutation
+    # often we will need input types for specific mutation
+    # in those cases we can define those input types in the mutation class itself\
+    description "Delete a subtask that belongs to a specific task"
+    argument :subtask_id, ID, required: true
+
+    field :subtask, TypesSubtask::SubtaskType, null: false
+
+    def resolve(subtask_id: nil)
+      subtask = Task.find_by(subtask_id)
+      subtask&.deactivated!
+
+      {subtask: subtask}
+    end
+  end
+end
